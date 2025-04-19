833374fbd3a87ceafdb16536364b6348364a6d05
BertozziPietro <b3rtozzipietro@gmail.com>
2025-04-19 10:27:24 +0200

    feat: bitboard improved

--- player/src/Bitboard.cpp
+++ player/src/Bitboard.cpp
@@ -1,152 +1,116 @@
 #include "Bitboard.hpp"
 
-// Getter per ottenere il bit alla posizione specifica (costante)
 bool Bitboard::get(int position) const {
-    if (position < 64) {
-        return (lower >> position) & 1;
-    } else {
-        return (upper >> (position - 64)) & 1;
-    }
+    if (position < 64) return (lower >> position) & 1;
+    return (upper >> (position - 64)) & 1;
 }
 
-// Operazione immutabile: Imposta il bit alla posizione specificata e restituisce una nuova bitboard
-Bitboard Bitboard::setC(int position) const {
+Bitboard Bitboard::setV(int position) const {
     Bitboard newBoard = *this;
-    if (position < 64) {
-        newBoard.lower |= (1ULL << position);
-    } else {
-        newBoard.upper |= (1ULL << (position - 64));
-    }
+    if (position < 64) newBoard.lower |= (1ULL << position);
+    else newBoard.upper |= (1ULL << (position - 64));
     return newBoard;
 }
 
-// Operazione immutabile: Negazione della bitboard e restituisce una nuova bitboard
-Bitboard Bitboard::notC() const {
+Bitboard Bitboard::notV() const {
     Bitboard newBoard = *this;
     newBoard.upper = ~newBoard.upper;
     newBoard.lower = ~newBoard.lower;
     return newBoard;
 }
 
-// Operazione immutabile: AND tra due bitboard e restituisce una nuova bitboard
-Bitboard Bitboard::andC(const Bitboard& other) const {
+Bitboard Bitboard::andV(const Bitboard& other) const {
     Bitboard newBoard = *this;
     newBoard.upper &= other.upper;
     newBoard.lower &= other.lower;
     return newBoard;
 }
 
-// Operazione immutabile: OR tra due bitboard e restituisce una nuova bitboard
-Bitboard Bitboard::orC(const Bitboard& other) const {
+Bitboard Bitboard::orV(const Bitboard& other) const {
     Bitboard newBoard = *this;
     newBoard.upper |= other.upper;
     newBoard.lower |= other.lower;
     return newBoard;
 }
 
-// Operazione immutabile: XOR tra due bitboard e restituisce una nuova bitboard
-Bitboard Bitboard::xorC(const Bitboard& other) const {
+Bitboard Bitboard::xorV(const Bitboard& other) const {
     Bitboard newBoard = *this;
     newBoard.upper ^= other.upper;
     newBoard.lower ^= other.lower;
     return newBoard;
 }
 
-// Shift a sinistra (immutabile): restituisce una nuova bitboard con i bit spostati
-Bitboard Bitboard::leftC(int positions) const {
+Bitboard Bitboard::leftV(int amount) const {
     Bitboard newBoard = *this;
-    if (positions < 64) {
-        // Shift della parte lower
-        newBoard.lower = (newBoard.lower << positions) | (newBoard.upper >> (64 - positions));
-        // Shift della parte upper
-        newBoard.upper = newBoard.upper << positions;
+    if (amount < 64) {
+        newBoard.upper = (newBoard.upper << amount) | (newBoard.lower >> (64 - amount));
+        newBoard.lower = newBoard.lower << amount;
     } else {
-        // Se lo shift è maggiore di 64, tutto va nell'upper
-        newBoard.upper = newBoard.lower << (positions - 64);
+        newBoard.upper = newBoard.lower << (amount - 64);
         newBoard.lower = 0;
     }
     return newBoard;
 }
 
-// Shift a sinistra (mutabile): modifica la bitboard in loco
-Bitboard& Bitboard::leftR(int positions) {
-    if (positions < 64) {
-        // Shift della parte lower
-        lower = (lower << positions) | (upper >> (64 - positions));
-        // Shift della parte upper
-        upper = upper << positions;
+Bitboard Bitboard::rightV(int amount) const {
+    Bitboard newBoard = *this;
+    if (amount < 64) {
+        newBoard.lower = (newBoard.lower >> amount) | (newBoard.upper << (64 - amount));
+        newBoard.upper = newBoard.upper >> amount;
     } else {
-        // Se lo shift è maggiore di 64, tutto va nell'upper
-        upper = lower << (positions - 64);
-        lower = 0;
+        newBoard.lower = upper >> (amount - 64);
+        newBoard.upper = 0;
     }
-    return *this;
+    return newBoard;
 }
 
-// Operazione mutabile: Imposta il bit alla posizione specificata e modifica la bitboard in loco
 Bitboard& Bitboard::set(int position) {
-    if (position < 64) {
-        lower |= (1ULL << position);
-    } else {
-        upper |= (1ULL << (position - 64));
-    }
+    if (position < 64) lower |= (1ULL << position);
+    else upper |= (1ULL << (position - 64));
     return *this;
 }
 
-// Operazione mutabile: Negazione della bitboard e modifica la bitboard in loco
 Bitboard& Bitboard::notR() {
     upper = ~upper;
     lower = ~lower;
     return *this;
 }
 
-// Operazione mutabile: AND tra due bitboard e modifica la bitboard in loco
 Bitboard& Bitboard::andR(const Bitboard& other) {
     upper &= other.upper;
     lower &= other.lower;
     return *this;
 }
 
-// Operazione mutabile: OR tra due bitboard e modifica la bitboard in loco
 Bitboard& Bitboard::orR(const Bitboard& other) {
     upper |= other.upper;
     lower |= other.lower;
     return *this;
 }
 
-// Operazione mutabile: XOR tra due bitboard e modifica la bitboard in loco
 Bitboard& Bitboard::xorR(const Bitboard& other) {
     upper ^= other.upper;
     lower ^= other.lower;
     return *this;
 }
 
-// Shift a destra (immutabile): restituisce una nuova bitboard con i bit spostati
-Bitboard Bitboard::rightC(int positions) const {
-    Bitboard newBoard = *this;
-    if (positions < 64) {
-        // Shift della parte upper
-        newBoard.upper = (newBoard.upper >> positions) | (newBoard.lower << (64 - positions));
-        // Shift della parte lower
-        newBoard.lower = newBoard.lower >> positions;
+Bitboard& Bitboard::leftR(int amount) {
+    if (amount < 64) {
+        upper = (upper << amount) | (lower >> (64 - amount));
+        lower = lower << amount;
     } else {
-        // Se lo shift è maggiore di 64, tutto va nel lower
-        newBoard.lower = upper >> (positions - 64);
-        newBoard.upper = 0;
+        upper = lower << (amount - 64);
+        lower = 0;
     }
-    return newBoard;
+    return *this;
 }
 
-// Shift a destra (mutabile): modifica la bitboard in loco
-Bitboard& Bitboard::rightR(int positions) {
-    if (positions < 64) {
-        // Shift della parte upper
-        upper = (upper >> positions) | (lower << (64 - positions));
-        // Shift della parte lower
-        lower = lower >> positions;
+Bitboard& Bitboard::rightR(int amount) {
+    if (amount < 64) {
+        lower = (lower >> amount) | (upper << (64 - amount));
+        upper = upper >> amount;
     } else {
-        // Se lo shift è maggiore di 64, tutto va nel lower
-        lower = upper >> (positions - 64);
+        lower = upper >> (amount - 64);
         upper = 0;
     }
     return *this;
--- player/src/Bitboard.hpp
+++ player/src/Bitboard.hpp
@@ -7,33 +7,31 @@
 
 class Bitboard {
 public:
-    // Costruttore che inizializza le due metà (upper e lower) della bitboard
     Bitboard(uint64_t upper = 0, uint64_t lower = 0) : upper(upper), lower(lower) {}
 
-    // Getter per ottenere il bit alla posizione specifica (costante)
     bool get(int position) const;
 
-    // Operazioni che non modificano la bitboard, ma ne restituiscono una nuova
-    Bitboard setC(int position) const;
-    Bitboard notC() const;
-    Bitboard andC(const Bitboard& other) const;
-    Bitboard orC(const Bitboard& other) const;
-    Bitboard xorC(const Bitboard& other) const;
-    Bitboard leftC(int positions) const;
-    Bitboard rightC(int positions) const;
+    // By Value
+    Bitboard setV(int position) const;
+    Bitboard notV() const;
+    Bitboard andV(const Bitboard& other) const;
+    Bitboard orV(const Bitboard& other) const;
+    Bitboard xorV(const Bitboard& other) const;
+    Bitboard leftV(int amount) const;
+    Bitboard rightV(int amount) const;
 
-    // Operazioni che modificano la bitboard in loco (e ritornano il riferimento a sé stessa)
+    // By Reference
     Bitboard& set(int position);
-    Bitboard& notR();  // Negazione della bitboard
-    Bitboard& andR(const Bitboard& other);  // AND tra due bitboard
-    Bitboard& orR(const Bitboard& other);   // OR tra due bitboard
-    Bitboard& xorR(const Bitboard& other);  // XOR tra due bitboard
-    Bitboard& leftR(int positions);  // Shift a sinistra
-    Bitboard& rightR(int positions); // Shift a destra
+    Bitboard& notR();
+    Bitboard& andR(const Bitboard& other);
+    Bitboard& orR(const Bitboard& other);
+    Bitboard& xorR(const Bitboard& other);
+    Bitboard& leftR(int amount);
+    Bitboard& rightR(int amount);
 
 private:
-    uint64_t upper;  // Parte superiore della bitboard
-    uint64_t lower;  // Parte inferiore della bitboard
+    uint64_t upper;
+    uint64_t lower;
 };
 
 #endif
